e6cfe1dbcbfaa47bef290701d65fec2949a6fbc4
Tar-Telperien <venicapiea@verizon.net>
2019-12-06 11:59:19 -0600

    Minor changes

--- TengwarFont/TengwarFont/TransliterateText.swift
+++ TengwarFont/TengwarFont/TransliterateText.swift
@@ -10,16 +10,27 @@ import UIKit
 import EFIconFont
 
 class TransliterateText: EFIconFontProtocol {
-    var name: String { get }
+    var name: String {
+        return "Tengwar Eldamar"
+    }
 
     // `path` is path of .ttf file
-    var path: String { get }
+    var path: String {
+        return "måns-björkman_tengwar-eldamar/tengel__.ttf"
+    }
 
     // `unicode` is unique identifier of particular icon
-    var unicode: String { get }
+    var unicode: String {
+        return "Mans Bjorkman: Tengwar Eldamar: 2007"
+    }
     
     func transliterateThis(string: String) -> NSAttributedString {
         var str = NSAttributedString(string: string)
+        return str
+    }
+    
+    func attributedString(size fontSize: CGFloat, attributes: [NSAttributedString.Key : Any]?) -> NSAttributedString? {
         
     }
 }
+
